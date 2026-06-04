e7fa0ce48dc295b8d5ae52fa428e0a00ec38b048
bildzeitung <bildzeitung@gmail.com>
2026-06-04 17:47:57 -0400

    Added Ollama

--- 3rdparty-install.sh
+++ 3rdparty-install.sh
@@ -6,3 +6,9 @@
 # RTK (https://github.com/rtk-ai/rtk)
 curl -fsSL https://raw.githubusercontent.com/rtk-ai/rtk/refs/heads/master/install.sh | sh
 
+# Ollama
+#
+curl -fsSL https://ollama.com/install.sh | sh
+ollama -v
+ollama pull qwen3-embedding:0.6b
+
